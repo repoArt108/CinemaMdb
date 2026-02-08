120e3b3dbb7b687c53df5d0f882a7d3e015c6635
Hazem Amin <cs.hazemelsayed@gmail.com>
2026-02-09 00:22:07 +0200

    vercel insights

--- cinema/src/components/Layout.jsx
+++ cinema/src/components/Layout.jsx
@@ -12,6 +12,7 @@ export default function Layout() {
         <Outlet/>
       </main>
       <Footer />
+      
     </div>
   );
 }
--- cinema/src/main.jsx
+++ cinema/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 import App from './App.jsx'
 import Context from './components/Context/Context.jsx'
@@ -9,6 +10,6 @@ createRoot(document.getElementById('root')).render(
     <Context>
       <App />
     </Context>
-    
+    <SpeedInsights/>
   </StrictMode>,
 )
